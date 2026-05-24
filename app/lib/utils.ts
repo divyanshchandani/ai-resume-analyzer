65c2f4e07273182813fe8bf3b8c34fde10c9f963
Divyansh Chandani <divyanshchandani40672@gmail.com>
2026-05-24 19:38:16 +0530

    These are the changes regarding the upload functionality for the Web Application

---
+++ app/lib/utils.ts
@@ -0,0 +1,9 @@
+export function formatSize(bytes: number): string {
+  if (bytes === 0) return '0 B';
+
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  const index = Math.floor(Math.log(bytes) / Math.log(1024));
+  const size = bytes / Math.pow(1024, index);
+
+  return `${size.toFixed(2).replace(/\.?0+$/, '')} ${units[index]}`;
+}
---
+++ app/routes/upload.tsx
@@ -0,0 +1,86 @@
+import React, { useState, type FormEvent } from "react";
+import Navbar from "~/components/Navbar";
+import FileUploader from "~/components/FileUploader";
+
+export default function Upload() {
+
+    const[isProcessing, setIsProcessing] = useState(false);
+    const[statusText, setStatusText] = useState('');
+    const [file, setFile] = useState<File | null>(null);
+    const handleFileSelect = (file: File|null) => {
+        setFile(file);
+    } 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget.closest('form')
+        if(!form) return;
+        const formData = new FormData(form);
+        const companyName = formData.get('company-name');
+        const jobTitle = formData.get('job-title');
+        const jobDescription = formData.get('job-description');
+        console.log({
+            companyName, jobTitle, jobDescription, file
+        })
+    }
+
+  return (
+    <main className="bg-[url('images/bg-main.svg')] bg-cover">
+        <Navbar />
+        <section className="main-section">
+            <div className="page-heading">
+                <h1>Smart feedback for your dream job</h1>
+                {isProcessing ? (
+                    <>
+                        <h2>{statusText}</h2>
+                        <img src="/images/resume-scan.gif" className="w-full" />
+                    </>
+                ) : (
+                    <h2>Drop your resume for an ATS Score & Imporvement Tips</h2>
+                )}
+                {!isProcessing && (
+                    <form 
+                        id="upload-form" 
+                        onSubmit={handleSubmit}
+                        className="flex flex-col gap-4 mt-8"
+                        >
+                            <div className="form-div ">
+                                <label htmlFor="company-name"><strong>Company Name</strong></label>
+                                <input 
+                                    type="text"
+                                    name="company-name"
+                                    placeholder="Company Name"
+                                    id="company-name"></input>
+                            </div>
+
+                            <div className="form-div ">
+                                <label htmlFor="job-title"><strong>Job Title</strong></label>
+                                <input 
+                                    type="text"
+                                    name="job-title"
+                                    placeholder="Job Title"
+                                    id="job-title"></input>
+                            </div>
+
+                            <div className="form-div ">
+                                <label htmlFor="job-description"><strong>Job Description</strong></label>
+                                <textarea 
+                                    rows={5}
+                                    name="job-description"
+                                    placeholder="Job Description"
+                                    id="job-description"></textarea>
+                            </div>
+
+                            <div className="form-div ">
+                                <label htmlFor="uploader"><strong>Upload Resume</strong></label>
+                                <FileUploader onFileSelect={handleFileSelect} selectedFile={file}/>
+                            </div>
+                            <button className="primary-button" type="submit">
+                                Analyse Resume
+                            </button>
+                        </form>
+                )}
+            </div>
+        </section>
+    </main>
+  );
+}
